bd07b9f43a03f37d1e69eddc5c31b4afcac87a7d
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-15 16:49:03 -0400

    backgground useeffect

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const PokeCard = (props) => {
   const [defaultURL, setDefaultURL] = useState("");
   const [shinyURL, setShinyURL] = useState("");
 
-  const onClickHandler = () => {
-    console.log("Fetch Info");
+  // 2 parameters
+  useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/" + props.pokemon)
       .then((res) => res.json())
       .then((res) => {
@@ -15,13 +16,17 @@ const PokeCard = (props) => {
         setShinyURL(res.sprites.front_shiny);
       })
       .catch((err) => console.log(err));
+  }, []);
+
+  const onClickHandler = () => {
+    console.log("Fetch Info");
   };
 
   return (
-    <div className="App">
+    <div className="poke-card">
       <h1>TEST </h1>
       <img src={defaultURL} alt={props.pokemon} />
-      <button onClick={onClickHandler}>get image</button>
+      <h2>{props.pokemon}</h2>
     </div>
   );
 };
